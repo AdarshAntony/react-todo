539f8cad842aa2e1a87998612442533fa6fd6391
AdarshAntony <137606306+AdarshAntony@users.noreply.github.com>
2023-07-09 13:09:25 +0530

    Simplyfied code

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,20 @@
-import React, {  useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { ITask } from './interfaces';
 
 //File Imports
 import './App.css';
-import Navbar from './components/navbar/Navbar';
-import Home from './components/pages/Home';
-import CreateTask from './components/pages/CreateTask';
+import Home from './components/pages/home/Home';
+import CreateTask from './components/pages/create-task/CreateTask';
 
 function App() {
-
-  const [task, setTask] = useState<any>("");
-  const [todoList, setTodoList] = useState<ITask[]>([]);
-
-  //AddTask Function
-  const addTask = () => {
-    const newTask = {
-      id: Math.floor(Math.random() * 10000),
-      taskName: task,
-    }
-    setTodoList([...todoList, newTask]);
-    setTask("");
-  };
-
-
-  //UpdateTask Function
-  const updateTask = (taskId: number, newTaskName: string) => {
-    const updatedList = todoList.map((task) => task.id === taskId ? { ...task, taskName: newTaskName } : task);
-    setTodoList(updatedList);
-  };
-
-  //DeleteTask Function
-  const deleteTask = (id: number) => {
-    setTodoList(todoList.filter((task) => {
-      return task.id !== id
-    }));
-  };
 
   return (
     <div className='App-body'>
       <div className='nav-bar'>
         <Router>
-          <Navbar></Navbar>
           <Routes>
-            <Route path='/' element={<Home deleteTask={deleteTask} updateTask={updateTask} task={task} todoList={todoList} />} ></Route>
-            <Route path='/createtask' element={<CreateTask task={task} addTask={addTask} setTask={setTask} />} ></Route>
+            <Route path='/' element={<Home />} ></Route>
+            <Route path='/createtask' element={<CreateTask />} ></Route>
 
           </Routes>
         </Router>
