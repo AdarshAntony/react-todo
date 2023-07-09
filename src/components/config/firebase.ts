08d1d788e04c83c2047a2adb8dec3a32f9c5424d
AdarshAntony <137606306+AdarshAntony@users.noreply.github.com>
2023-07-09 13:08:09 +0530

    database config added

---
+++ src/components/config/firebase.ts
@@ -0,0 +1,24 @@
+//Paste your firebase config here.
+
+//config format should be as follows
+
+/*
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: ",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app) 
+*/
